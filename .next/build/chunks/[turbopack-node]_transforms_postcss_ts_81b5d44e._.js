e51958e5f2a1dcab9f6764e807aaf1008cf2dad9
vamsi-akiri <akirivamsi@gmail.com>
2026-03-13 07:44:00 +0530

    Add files via upload

---
+++ .next/build/chunks/[turbopack-node]_transforms_postcss_ts_81b5d44e._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/web/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "build/chunks/e176a_97471aa5._.js",
+  "build/chunks/[root-of-the-server]__3f599006._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/web/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
